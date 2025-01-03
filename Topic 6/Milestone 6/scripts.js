fce8ec9f228770cfeba03a6f11a31b6f1fb0eb79
Dimitri Jimenez <dimijimz@Dimitris-MacBook-Air.local>
2025-01-03 17:05:38 -0500

    Some more Topic 6

--- Topic 6/Milestone 6/scripts.js
+++ Topic 6/Milestone 6/scripts.js
@@ -1,12 +1,3 @@
-// Function to toggle background color on button click
-function changeBackgroundColor() {
-    if (document.body.style.backgroundColor === "purple") {
-        document.body.style.backgroundColor = "white";
-    } else {
-        document.body.style.backgroundColor = "purple";
-    }
-}
-
 // Function to make "key" bold and larger when hovered
 function enlargeKey(element) {
     element.style.fontWeight = "bold";
