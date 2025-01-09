d4a4eb584a943b55552f4fb5bb6aa5680149cb93
Dimitri Jimenez <dimijimz96@gmail.com>
2025-01-09 11:07:45 -0500

    Fixed path for fragments folder and updated project structure

--- Topic 7/js/copyright.js
+++ Topic 7/js/copyright.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    $("#copyright").load("../fragments/_copyright.html");
-});
+    console.log("Running the load function.");
+    $("#copyright").load("fragments/_copyright.html", function (response, status, xhr) {
+        if (status === "error") {
+            console.error("Error loading the copyright file:", xhr.status, xhr.statusText);
+        } else {
+            console.log("Loaded copyright content successfully.");
+        }
+    });
+}); 
